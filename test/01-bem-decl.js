f144c0506ba9847adbec663e8a435010ad81e23f
Anton Gerasimov <a.gerasimov@reg.ru>
2014-06-26 15:34:55 +0600

    stub for mocha

---
+++ test/01-bem-decl.js
@@ -0,0 +1,19 @@
+//
+// bem-decl.js tests
+//
+var revision = require('../tasks/lib/bem-decl'),
+    should = require('should');
+
+require('mocha');
+
+describe('bem-decl', function() {
+
+    describe('foo()', function() {
+
+        it('should equal to foo', function() {
+            'foo'.should.be.eql('foo');
+        });
+
+    });
+
+});
