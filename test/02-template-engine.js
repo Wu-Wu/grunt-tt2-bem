3c9130956a81352c1f1d93b0f2cfd65ac3b3bd52
Anton Gerasimov <a.gerasimov@reg.ru>
2014-07-04 20:14:34 +0600

    added tt2 directives handling (draft)

--- test/02-template-engine.js
+++ test/02-template-engine.js
@@ -3,16 +3,28 @@
 // template-engine.js tests
 //
 var TemplateEngine = require('../lib/template-engine'),
-    te = new TemplateEngine({ debug : true }),
+    te = new TemplateEngine({ debug : true, root: 'test/fixtures' }),
     should = require('should'),
     fs = require('fs');
 
 require('mocha');
 
+var loadFixture = function (name) {
+    return {
+        template: fs.readFileSync('test/fixtures/' + name + '.html', {encoding: 'utf8'})
+        // expected: JSON.parse(fs.readFileSync('test/fixtures/'+name+'.json', {encoding: 'utf8'}))
+    };
+};
+
 describe('template-engine', function(){
 
     it('should be true', function(){
+        var aaa = loadFixture('tt2-base');
+
+        te.process(aaa.template);
+
         true.should.be.eql(true);
     });
+
 
 });
